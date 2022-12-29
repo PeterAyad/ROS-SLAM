1a4999e736e3a0939bc85fc19512e16e4dce010a
peterayad <peter.ayad00@eng-st.cu.edu.eg>
2022-12-29 21:52:23 +0200

    initial commit

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+sed -i -e  '104d;105d' src/summit_xl_sim/summit_xl_gazebo/launch/summit_xl_one_robot.launch
 roscore &
 source devel/setup.bash 
 roslaunch launch_pkg all.launch
-sed -i -e  '104d;105d' src/summit_xl_sim/summit_xl_gazebo/launch/summit_xl_one_robot.launch

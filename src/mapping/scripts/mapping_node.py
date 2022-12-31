a5fb4ac598e36388ce8410fd532e4066f675c82f
Andrew Boshra <andrew.boshr@gmail.com>
2022-12-31 22:18:09 +0200

    refactored mapping

--- src/mapping/scripts/mapping_node.py
+++ src/mapping/scripts/mapping_node.py
@@ -3,151 +3,151 @@
 import numpy as np
 import message_filters
 import rospy
+
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import OccupancyGrid
 from nav_msgs.msg import Odometry
 from laser_geometry import LaserProjection
-from sensor_msgs.msg import LaserScan, PointCloud2
-from tf2_sensor_msgs.tf2_sensor_msgs import do_transform_cloud
+from sensor_msgs.msg import LaserScan
 from tf2_ros import Buffer, TransformListener
 from sensor_msgs import point_cloud2
 from laser_geometry import LaserProjection
-# from cmp_robot.msg import Data
+from utils import log_2_probability,probability_2_log , is_inside_mat
+
+from skimage.draw import line
 
 
-def l2p(l):
-    """Convert log-odds to probability."""
-    return 1 - (1/(1+np.exp(l)))
-
-# l(x) = log(\frac{p(x)}{1 - p(x)})
-def p2l(p):
-    """Convert probability to log-odds."""
-    return np.log(p/(1-p))
-
-
-def is_inside (i, j):
-    """Check if grid coordinates are inside the map."""
-    return i < world.shape[0] and j < world.shape[1] and i>=0 and j>=0
-
-def bresenham (i0, j0, i1, j1, d):   # i0, j0 (starting point)
-    """Bresenham's line algorithm."""
-    dx = np.absolute(j1-j0)
-    dy = -1 * np.absolute(i1-i0)
-    sx = -1
-    if j0<j1:
-        sx = 1
-    sy = -1
-    if i0<i1:
-        sy = 1
-    jp, ip = j0, i0
-    err = dx+dy                     # error value e_xy
-    while True:                     # loop
-        if (jp == j1 and ip == i1) or (np.sqrt((jp-j0)**2+(ip-i0)**2) >= d) or not is_inside(ip, jp):
-            return ip, jp, False
-        elif world[int(ip),int(jp)]==100:
-            return ip, jp, True
-
-        if is_inside(ip, jp):
-            # miss:
-            world[int(ip),int(jp)] += sensor_model_l_free - sensor_model_l_prior
-
-        e2 = 2*err
-        if e2 >= dy:                # e_xy+e_x > 0 
-            err += dy
-            jp += sx
-        if e2 <= dx:                # e_xy+e_y < 0
-            err += dx
-            ip += sy
-
-
-
-def get_transform_cloud(source_frame, target_frame):
-    """Get the transform from the robot frame to the laser frame."""
-    # get the transform from the robot frame to the laser frame
-    cloud = LaserProjection().projectLaser(source_frame)
-    transform = tf_buffer.lookup_transform(target_frame.header.frame_id, source_frame.header.frame_id.replace("/",""), rospy.Time())
-    pcd2_laser = do_transform_cloud(cloud, transform) #returns PointCloud2
-    world_points = list(point_cloud2.read_points(pcd2_laser, field_names=("x", "y", "z"), skip_nans=True))
-    # rospy.loginfo(world_points)
+def laser_world_points(source_laser_frame):
+    cloud = LaserProjection().projectLaser(source_laser_frame)
+    world_points = list(point_cloud2.read_points(cloud, field_names=("x", "y"), skip_nans=True))
     return world_points
 
-def quarternion_to_yaw(q_x, q_y, q_z, q_w):
-    """Convert quarternion to yaw angle."""
-    siny_cosp = 2 * (q_w * q_z + q_x * q_y)
-    cosy_cosp = 1 - 2 * (q_y * q_y + q_z * q_z)
-    return np.arctan2(siny_cosp, cosy_cosp)
+class Map:
+    frame_id = 'robot_map'
+    def __init__(self,width,height,resolution):
+        self.width      = width
+        self.height     = height
+        self.resolution = resolution
+        
+        self.world = sensor_model_l_prior * np.ones((self.width_in_pxs, self.height_in_pxs), dtype=np.float32)
+
+        self.origin_x=-self.width//2
+        self.origin_y=-self.height//2
+    @property
+    def width_in_pxs(self):
+        return int(self.width/self.resolution)
+
+    @property
+    def height_in_pxs(self):
+        return int(self.height/self.resolution)
+
+    def _to_map_pixels(self,x,y):    
+        return int((x-self.origin_x)/self.resolution) , int((y-self.origin_y)/self.resolution)
+    
+    def draw_line(self,x0,y0,x1,y1):
+        """
+            updates the propabilities of the points in line 
+            starting from x0,y0 to x1,y1 in the world matrix
+        """
+        x0,y0= self._to_map_pixels(x0,y0)
+        x1,y1= self._to_map_pixels(x1,y1)
+        
+        rr,cc = line(x0,y0,x1,y1)
+
+        # check if points inside the map
+        
+        valid_points = (rr < self.world.shape[0]) & (rr >= 0) & (cc < self.world.shape[1]) & (cc >= 0)
+        
+        rr = rr[valid_points]
+        cc = cc[valid_points]
+
+        # free points
+        self.world[rr,cc] += sensor_model_l_free - sensor_model_l_prior
+        
+        if len(rr)==0:
+            return 0,0
+        
+        # return the last point of the line
+        return rr[-1], cc[-1]
+    
+    def as_occ_grid(self,stamp,frame_id='robot_map'):
+        map_occ = OccupancyGrid()
+        map_occ.header.frame_id = frame_id
+        map_occ.info.resolution = self.resolution
+
+        map_occ.info.width = self.width_in_pxs
+        map_occ.info.height = self.height_in_pxs
+        map_occ.info.origin.position.x = self.origin_x
+        map_occ.info.origin.position.y = self.origin_y
+        map_occ.header.stamp = stamp
+
+        gridmap = self.world.copy()
+        gridmap = gridmap.T
+        gridmap_p = log_2_probability(gridmap)
+        gridmap_int8 = (gridmap_p*100).astype(dtype=np.int8)
+        map_occ.data = gridmap_int8.flatten()
+        return map_occ
 
 
-def callback_handler(front_scan,  odom):
-    """callback function handler for the time synchronizer"""
-    rospy.loginfo("Callback handler called")
+
+def mapping_callback(scan,  odom):
+    rospy.loginfo("Mapping Callback Handler Called")
 
     global resolution
-    world_points = get_transform_cloud(front_scan, odom)
-    # extract the data from the messages
-  
-    map_msg = OccupancyGrid()
-    map_msg.header.frame_id = 'robot_map'
-    map_msg.info.resolution = resolution
-    map_msg.info.width = int(50/resolution)
-    map_msg.info.height = int(50/resolution)
-    map_msg.info.origin.position.x = -25
-    map_msg.info.origin.position.y = -25
-    map_msg.header.stamp = front_scan.header.stamp
+
+    world_points = laser_world_points(scan)
     world_points = np.array(world_points).astype(dtype=np.float32)
+    
     # put the world points in the world array
     # loop over the world_points
     for i, point in enumerate(world_points):
-        d = front_scan.ranges[i] / resolution
-        # rospy.loginfo(int(point[0]))
-        # rospy.loginfo(int(point[1]))
-        ip, jp, is_hit = bresenham(int((odom.pose.pose.position.x+25)/resolution), int((odom.pose.pose.position.y+25)/resolution), int((point[0]+25)/resolution), int((point[1]+25)/resolution), d)
-        if not np.isnan(d) and d != 30 and is_inside(int(ip),int(jp)):
-            # rospy.loginfo(ip)
-            # rospy.loginfo(jp)
-            world[int(ip),int(jp)] += sensor_model_l_occ - sensor_model_l_prior
-    gridmap = world.copy()
-    gridmap = gridmap.T
-    gridmap_p = l2p(gridmap)
-    gridmap_int8 = (gridmap_p*100).astype(dtype=np.int8)
-    map_msg.data = gridmap_int8.flatten()
-    grid_pub.publish(map_msg)
-    rospy.loginfo("Published the map")
+        
+        d = scan.ranges[i] / resolution
+        
+        robot_x = odom.pose.pose.position.x 
+        robot_y = odom.pose.pose.position.y 
+
+        y, x = map.draw_line(robot_x, robot_y, point[0], point[1])
+        
+        if not np.isnan(d) and d != 30 and is_inside_mat(y,x,map.world):
+            map.world[y,x] += sensor_model_l_occ - sensor_model_l_prior
+
+    occ_grid_publisher.publish(map.as_occ_grid(stamp=scan.header.stamp))
+    rospy.loginfo("[MappingModule] Published the map")
     
 
-# resolution = 0.25 # for fast mapping
-resolution = 0.02 # for high resolution mapping
+resolution = 0.25 # for fast mapping
+# resolution = 0.02 # for high resolution mapping
 
 
 if __name__ == '__main__':
-    sensor_model_l_occ = p2l(0.75)
-    sensor_model_l_prior = p2l(0.5)
-    sensor_model_l_free = p2l(0.45)
-    world = sensor_model_l_prior * np.ones((int(50/resolution), int(50/resolution)), dtype=np.float32)
-    rospy.init_node('SensorModule', anonymous=True)
+   
+    sensor_model_l_occ = probability_2_log(0.75)
+    sensor_model_l_prior = probability_2_log(0.5)
+    sensor_model_l_free = probability_2_log(0.45)
+
+    rospy.init_node('MappingModule', anonymous=True)
+
+    map = Map(100,100,resolution)
+    
     tf_buffer = Buffer()
     tf_listener = TransformListener(tf_buffer)
-    rospy.loginfo("SensorModule node started")
-    # Create a subscriber to the front laser scan topic
-    front_laser_sub = message_filters.Subscriber('/scan_multi', LaserScan)
+    rospy.loginfo("[MappingModule] node started")
+    
+    # subscriber to the laser scan topic
+    laser_sub = message_filters.Subscriber('/scan_multi', LaserScan)
 
-    # Create a subscriber to the rear laser scan topic
-    # rear_laser_sub = message_filters.Subscriber('/robot/rear_laser/scan', LaserScan)
+    #publisher to the occupancy grid topic
+    occ_grid_publisher = rospy.Publisher('/robot/map', OccupancyGrid, queue_size=10)
 
-    # Create a publisher to the occupancy grid topic
-    grid_pub = rospy.Publisher('/robot/map', OccupancyGrid, queue_size=10)
-
-    # Create a subscriber to the odometry topic
+    # subscriber to the odometry topic
     odom_sub = message_filters.Subscriber('/robot/robotnik_base_control/odom', Odometry)
 
-    # Create a publisher to the data topic
-    # data_pub = rospy.Publisher('/ourTopic', Data, queue_size=10)
-
     # Create a time synchronizer
-    ts = message_filters.ApproximateTimeSynchronizer([front_laser_sub, odom_sub],
+    ts = message_filters.ApproximateTimeSynchronizer([laser_sub, odom_sub],
                                                     10, 0.1, allow_headerless=True)
 
-    # Register a callback with the time synchronizer
-    ts.registerCallback(callback_handler)
+    ts.registerCallback(mapping_callback)
+
     while not rospy.is_shutdown():
         rospy.spin()

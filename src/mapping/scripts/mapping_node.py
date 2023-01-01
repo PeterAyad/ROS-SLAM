682fa6ad528a3cba65f30ea04f4eb84a4dc9f7c0
peterayad <peter.ayad00@eng-st.cu.edu.eg>
2023-01-01 14:45:17 +0200

    get_pose added

--- src/mapping/scripts/mapping_node.py
+++ src/mapping/scripts/mapping_node.py
@@ -12,27 +12,32 @@ from sensor_msgs.msg import LaserScan
 from tf2_ros import Buffer, TransformListener
 from sensor_msgs import point_cloud2
 from laser_geometry import LaserProjection
-from utils import log_2_probability,probability_2_log , is_inside_mat
+from utils import log_2_probability, probability_2_log, is_inside_mat
 
 from skimage.draw import line
 
 
 def laser_world_points(source_laser_frame):
     cloud = LaserProjection().projectLaser(source_laser_frame)
-    world_points = list(point_cloud2.read_points(cloud, field_names=("x", "y"), skip_nans=True))
+    world_points = list(point_cloud2.read_points(
+        cloud, field_names=("x", "y"), skip_nans=True))
     return world_points
+
 
 class Map:
     frame_id = 'robot_map'
-    def __init__(self,width,height,resolution):
-        self.width      = width
-        self.height     = height
-        self.resolution = resolution
-        
-        self.world = sensor_model_l_prior * np.ones((self.width_in_pxs, self.height_in_pxs), dtype=np.float32)
 
-        self.origin_x=-self.width//2
-        self.origin_y=-self.height//2
+    def __init__(self, width, height, resolution):
+        self.width = width
+        self.height = height
+        self.resolution = resolution
+
+        self.world = sensor_model_l_prior * \
+            np.ones((self.width_in_pxs, self.height_in_pxs), dtype=np.float32)
+
+        self.origin_x = -self.width//2
+        self.origin_y = -self.height//2
+
     @property
     def width_in_pxs(self):
         return int(self.width/self.resolution)
@@ -41,36 +46,37 @@ class Map:
     def height_in_pxs(self):
         return int(self.height/self.resolution)
 
-    def _to_map_pixels(self,x,y):    
-        return int((x-self.origin_x)/self.resolution) , int((y-self.origin_y)/self.resolution)
-    
-    def draw_line(self,x0,y0,x1,y1):
+    def _to_map_pixels(self, x, y):
+        return int((x-self.origin_x)/self.resolution), int((y-self.origin_y)/self.resolution)
+
+    def draw_line(self, x0, y0, x1, y1):
         """
             updates the propabilities of the points in line 
             starting from x0,y0 to x1,y1 in the world matrix
         """
-        x0,y0= self._to_map_pixels(x0,y0)
-        x1,y1= self._to_map_pixels(x1,y1)
-        
-        rr,cc = line(x0,y0,x1,y1)
+        x0, y0 = self._to_map_pixels(x0, y0)
+        x1, y1 = self._to_map_pixels(x1, y1)
+
+        rr, cc = line(x0, y0, x1, y1)
 
         # check if points inside the map
-        
-        valid_points = (rr < self.world.shape[0]) & (rr >= 0) & (cc < self.world.shape[1]) & (cc >= 0)
-        
+
+        valid_points = (rr < self.world.shape[0]) & (
+            rr >= 0) & (cc < self.world.shape[1]) & (cc >= 0)
+
         rr = rr[valid_points]
         cc = cc[valid_points]
 
         # free points
-        self.world[rr,cc] += sensor_model_l_free - sensor_model_l_prior
-        
-        if len(rr)==0:
-            return 0,0
-        
+        self.world[rr, cc] += sensor_model_l_free - sensor_model_l_prior
+
+        if len(rr) == 0:
+            return 0, 0
+
         # return the last point of the line
         return rr[-1], cc[-1]
-    
-    def as_occ_grid(self,stamp,frame_id='robot_map'):
+
+    def as_occ_grid(self, stamp, frame_id='robot_map'):
         map_occ = OccupancyGrid()
         map_occ.header.frame_id = frame_id
         map_occ.info.resolution = self.resolution
@@ -89,6 +95,9 @@ class Map:
         return map_occ
 
 
+def get_robot_position(odom):
+    return odom.pose.pose.position.x, odom.pose.pose.position.y
+
 
 def mapping_callback(scan,  odom):
     rospy.loginfo("Mapping Callback Handler Called")
@@ -97,56 +106,57 @@ def mapping_callback(scan,  odom):
 
     world_points = laser_world_points(scan)
     world_points = np.array(world_points).astype(dtype=np.float32)
-    
+
     # put the world points in the world array
     # loop over the world_points
     for i, point in enumerate(world_points):
-        
+
         d = scan.ranges[i] / resolution
-        
-        robot_x = odom.pose.pose.position.x 
-        robot_y = odom.pose.pose.position.y 
+
+        robot_x, robot_y = get_robot_position(odom)
 
         y, x = map.draw_line(robot_x, robot_y, point[0], point[1])
-        
-        if not np.isnan(d) and d != SENSOR_MAX_RANGE and is_inside_mat(y,x,map.world):
-            map.world[y,x] += sensor_model_l_occ - sensor_model_l_prior
+
+        if not np.isnan(d) and d != SENSOR_MAX_RANGE and is_inside_mat(y, x, map.world):
+            map.world[y, x] += sensor_model_l_occ - sensor_model_l_prior
 
     occ_grid_publisher.publish(map.as_occ_grid(stamp=scan.header.stamp))
     rospy.loginfo("[MappingModule] Published the map")
-    
 
-resolution = 0.25 # for fast mapping
+
+resolution = 0.25  # for fast mapping
 # resolution = 0.02 # for high resolution mapping
 
 SENSOR_MAX_RANGE = 30
 
 if __name__ == '__main__':
-   
+
     sensor_model_l_occ = probability_2_log(0.75)
     sensor_model_l_prior = probability_2_log(0.5)
     sensor_model_l_free = probability_2_log(0.45)
 
     rospy.init_node('MappingModule', anonymous=True)
 
-    map = Map(100,100,resolution)
-    
+    map = Map(100, 100, resolution)
+
     tf_buffer = Buffer()
     tf_listener = TransformListener(tf_buffer)
     rospy.loginfo("[MappingModule] node started")
-    
+
     # subscriber to the laser scan topic
     laser_sub = message_filters.Subscriber('/scan_multi', LaserScan)
 
-    #publisher to the occupancy grid topic
-    occ_grid_publisher = rospy.Publisher('/robot/map', OccupancyGrid, queue_size=10)
+    # publisher to the occupancy grid topic
+    occ_grid_publisher = rospy.Publisher(
+        '/robot/map', OccupancyGrid, queue_size=10)
 
     # subscriber to the odometry topic
-    odom_sub = message_filters.Subscriber('/robot/robotnik_base_control/odom', Odometry)
+    odom_sub = message_filters.Subscriber(
+        '/robot/robotnik_base_control/odom', Odometry)
 
     # Create a time synchronizer
     ts = message_filters.ApproximateTimeSynchronizer([laser_sub, odom_sub],
-                                                    10, 0.1, allow_headerless=True)
+                                                     10, 0.1, allow_headerless=True)
 
     ts.registerCallback(mapping_callback)
 
